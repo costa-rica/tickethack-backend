ed763908c8fa74602590a9e0d8720d28a711d2f9
costa-rica <nrodrig1@gmail.com>
2024-09-24 12:18:29 +0200

    commencé POST /search

--- routes/routerTrips.js
+++ routes/routerTrips.js
@@ -2,16 +2,34 @@ var express = require('express');
 var router = express.Router();
 const Trip = require('../models/trips')
 
+
 router.get('/search',(req,res) =>{
     // code
     console.log('- in /search --')
-
-
-    Trip.find().then(data =>{
+    Trip.find({})
+    .then(data =>{
         res.json({allTrips: data})
     })
+    // res.json({allTrips: true});
+});
 
+router.post('/search',(req,res) =>{
+    
+    console.log("- in POST /search")
+    
+    // const { arrival, departure, date } = req.body
 
+    const arrival = req.body.arrival
+    const departure = req.body.departure
+    const date = req.body.date
+    
+    console.log(``)
+    // code
+    console.log('- in /search --')
+    Trip.find({})
+    .then(data =>{
+        res.json({allTrips: data})
+    })
     // res.json({allTrips: true});
 });
 
